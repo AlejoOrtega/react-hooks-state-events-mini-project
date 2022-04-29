ff500431264c285465d8a5138660414eb6f08c91
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-04-29 13:17:41 -0400

    Done.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({task, onEliminateTask}) {
+  const {category, text} = task
+  const onDelete = () => {
+    onEliminateTask(text)
+  }
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text" name='text'>{text}</div>
+      <button className="delete" onClick={onDelete}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task";
+import {v4 as uuid} from 'uuid'
 
-function TaskList() {
+function TaskList({tasks, onEliminateTask}) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task)=> {
+        if(task.hidden===false){
+          return <Task key={uuid()} task={task} onEliminateTask={onEliminateTask}/>
+        }
+        return false
+      } )}
     </div>
   );
 }
